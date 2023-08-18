0340f4f9dacdfe832d36efc114d4765cf48be54e
songboyao <boyao.song@huawei.com>
2023-08-18 16:05:17 +0100

    Update

--- after/ftplugin/cpp.lua
+++ after/ftplugin/cpp.lua
@@ -2,23 +2,7 @@ local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
     { name = "clang_format" }
 }
--- local notify = vim.notify
--- vim.notify = function(msg, ...)
---   if msg:match("warning: multiple different client offset_encodings detected for buffer, this is not supported yet") then
---     return
---   end
-
---   notify(msg, ...)
--- end
-
 vim.keymap.set('n', 'gh', ':ClangdSwitchSourceHeader<CR>')
-
-local linters = require "lvim.lsp.null-ls.linters"
-linters.setup {
-    {
-        name = "cpplint",
-    }
-}
 if os.getenv("USE_2_INDENT") then
     vim.o.tabstop = 2
     vim.opt.shiftwidth = 2
